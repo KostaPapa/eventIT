73af1066324dda0e624b471c2c6ffbf4d075b9b8
Gryczka <PatrickGryczka@gmail.com>
2014-12-10 19:49:18 -0500

    Displaying upcoming events
    Displaying upcoming events

--- eventIT/events.php
+++ eventIT/events.php
@@ -1,24 +1,44 @@
-<div class="container" style="float: left;">
+<div class="container" >
   <div class="row">
-
-          <div class="col-lg-4">
-
-            <img class="img-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" style="width: 140px; height: 140px;">
-            <p></p>
-            <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
-          
-          </div><!-- /.col-lg-4 -->
-          <div class="col-lg-4">
-            <img class="img-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" style="width: 140px; height: 140px;">
-            <p></p>
-            <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
-          
-          </div><!-- /.col-lg-4 -->
-          <div class="col-lg-4">
-            <img class="img-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" style="width: 140px; height: 140px;">
-            <p></p>
-            <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
-          </div><!-- /.col-lg-4 -->
-
+		<style>
+ 			.event { background-color:#FFE5F9; margin-bottom: 10px;}
+			.eventName{ color: #601286; font-size: 40px; float: left;}
+			.clubName{color: white; font-size: 40px;background-color: #601286; float:right; margin-right: -15px; padding-left: 5px; padding-right: 5px;}
+			.clear{clear: both;}
+			.UpcomingHeading{color: #601286; font-size: 60px;font-weight: bold;}
+			.floatRight{float:right;}
+		</style>	
+		<?php
+			include "mysql.php";
+			//querying events that in ascending order, but only events who's date is in the future
+			$query = "select cname, ename, edate, location, description
+								from event
+									where edate > ?
+										order by edate asc";
+			$datetime = new DateTime();
+			$date = $datetime->format('Y-m-d H:i:s');
+			$stmt = $mysqli->prepare($query);
+			$stmt->bind_param('s', $date);
+			echo"<div class=\"UpcomingHeading\">Upcoming Events</div>";
+			//print_r($date);
+			$stmt->execute();
+			$stmt->bind_result($cname, $ename, $edate, $location, $description);
+			while($stmt->fetch()){
+				if(strnatcmp($date, $edate) <= 0){
+					echo"<div class=\"col-md-6 col-md-offset-3 col-lg-12 col-lg-offset-0 event\">";
+					echo"<div class=\"eventName\">$ename</div>";
+					echo"<div class=\"clubName\">$cname</div>";
+					echo"<div class=\"clear\"></div>";
+					echo"<div class=\"eventDate\"><b>Time: </b>$edate</div>";
+					echo"<div class=\"eventLocation\"><b>Location: </b>$location</div>";
+					echo"<div class=\"eventDescription\"><b>Description: </b>$description</div>";
+					
+					//The Button we'll eventually need to add a link to the events through
+					echo"<p><a class=\"btn btn-default floatRight\" href=\"#\" role=\"button\">View details &raquo;</a></p><div class=\"clear\"></div>";
+					
+					echo"</div><!-- /.col-md-6 -->";
+				}
+			}
+		?>
   </div><!-- /.row --> 
 </div><!-- /.container --> 
